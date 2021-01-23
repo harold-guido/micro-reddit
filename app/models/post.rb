68ab306c36ed2486bb3f00b245ddf2ae62516808
Harold Parra <gc.harold.parra@gmail.com>
2021-01-23 16:11:32 +0000

    Add validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :title, 
+    exists: true, 
+    length: { maximum: 24 }
+    format: { has: /[A-Za-z ]/, message: "only allows letters"}
+
+  validates :body, 
+    exists: true, 
+    length: { minimum: 12 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  validates :user_name, presence: true, length: { maximum: 12 }
+  validates :user_name, 
+    presence: true, 
+    length: { maximum: 12 },
+    format: { add: /[A-Za-z]/, message: "only allows letters" }
 end
